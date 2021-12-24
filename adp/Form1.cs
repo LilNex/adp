28a5ff883ddc2c894d0a2677c648d17d79fe551e
LilNex <nextorchq@gmail.com>
2021-12-24 18:54:33 +0100

    Connection ajouté

--- adp/Form1.cs
+++ adp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,57 @@ namespace adp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadEvent(object sender, EventArgs e)
+        {
+            string txt = "non";
+            label1.Text = txt;
+            button1.Text = "Ajouter";
+            button2.Text = "Modifier";
+            label1.ForeColor = Color.Red;
+
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            clsDb.Ajouter(textBox1.Text);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strConn = @"Data Source=LILNEX\SQLEXPRESS;Initial Catalog=lilnx;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            string command = "update stagaire set nom where id";
+            command = "update stagiaire set nom ='" + textBox1.Text + "' where id= " + txtId.Text;
+
+            SqlCommand cmd = new SqlCommand(command);
+            cmd.Connection = conn;
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Ajouté avec succes");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ adp/clsDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adp
+{
+    class clsDb
+    {
+        public static SqlConnection connectDb()
+        {
+            string strConn = @"Data Source=LILNEX\SQLEXPRESS;Initial Catalog=lilnx;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            return conn;
+            
+        }
+
+        public static void Ajouter(string nom)
+        {
+            string strcmd = "INSERT INTO stagiaire VALUES ('" + nom + "')";
+            SqlCommand cmd = new SqlCommand(strcmd);
+            SqlConnection conn = connectDb();
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+           
+        }
+
+
+    }
+}
